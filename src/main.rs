4b5730bd917af2baddda33892fba046bae065e85
Valery Meleshkin <valery@meleshk.in>
2022-01-08 22:43:33 +0100

    Pulling in glam as a vector math lib. End of chapter 3.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::io::{self, Write};
 
+type ImagePixel = image::Rgb<u8>;
+type OutputImage = image::RgbImage;
+
+type Color = glam::Vec3;
+type Point3 = glam::Vec3;
+
+fn output_color(px: &mut ImagePixel, c: Color) {
+    fn f32_to_u8(v: f32) -> u8 {
+        (v * 255.999) as u8
+    }
+
+    *px = image::Rgb([f32_to_u8(c[0]), f32_to_u8(c[1]), f32_to_u8(c[2])])
+}
+
 fn main() {
     let image_width: u32 = 256;
     let image_height: u32 = 256;
-    let mut img_buf = image::RgbImage::new(image_width, image_height);
-
-    fn f64_to_u8(c: f64) -> u8 {
-        (c * 255.999) as u8
-    }
+    let mut img_buf = OutputImage::new(image_width, image_height);
 
     for j in 0..image_height {
         let scanline = image_height - j - 1;
@@ -15,12 +25,12 @@ fn main() {
         io::stderr().flush().unwrap();
 
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.25;
-
-            let px = img_buf.get_pixel_mut(i, scanline);
-            *px = image::Rgb([f64_to_u8(r), f64_to_u8(g), f64_to_u8(b)])
+            let color = Color::new(
+                i as f32 / (image_width - 1) as f32,
+                j as f32 / (image_height - 1) as f32,
+                0.25,
+            );
+            output_color(img_buf.get_pixel_mut(i, scanline), color);
         }
     }
 
